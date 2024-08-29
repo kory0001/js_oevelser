23e3af2fba43d8041e0acbc9f40f3fca642f6d5b
Kornelia <kory0001@stud.kea.dk>
2024-08-29 11:03:38 +0200

    js_oevelser

---
+++ superhelt_objekt.js
@@ -0,0 +1,7 @@
+const superhelt = {
+  navn: "Hurtige Helle",
+  superkraft: "Kan løbe 100km/timen",
+  beklæding: "kappe og maske",
+  højde: "148 cm",
+  livretter: ["pasta med kødsovs", "stegt flæsk med persillesovs", "hjemmerørt tatar"],
+};
